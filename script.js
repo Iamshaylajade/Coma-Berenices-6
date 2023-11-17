248504181fe043f1ffc66fce2cbd90bbafa971f7
iamshaylajade <shaylajade21@yahoo.com>
2023-11-17 05:07:08 -0500

    updated script.js

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ $(document).ready(function) () {
 
     const apiKey =
 
-    const cityE1 = $('h2#city');
-    const dateE1 = $('h3#date');
-    const weatherIconE1 = $('img#weather-icon');
-    const temperatureE1 = $('span#temperature');
-    const humidityE1 = $('span#humidity');
-    const windE1 = $('span#wind');
-    const cityListE1 = ('div.cityList');
-    const cityInput = $('#city-input')
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
 
     let pastSearches = [];
 
